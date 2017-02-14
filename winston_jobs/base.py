6fbf55dd0fb9d3b53aea366931303aba73659cf6
David Horn <david.horn@disney.com>
2017-02-13 20:32:31 -0800

    helper function to make event

--- winston_jobs/base.py
+++ winston_jobs/base.py
@@ -1,9 +1,21 @@
 import models
+from winston_api.models import Event, Source
+from django.utils.timezone import now, localtime
 
 
 class Job(object):
     def _type(self):
         return self.__class__.__name__
+
+    def make_event(self, msg, when=now()):
+        source, created = Source.objects.get_or_create(
+            name=self._type()
+        )
+        Event.objects.create(
+            source=source,
+            message=msg,
+            scheduled_time=localtime(when)
+        )
 
     def __init__(self):
         self.job_model, created = models.Job.objects.get_or_create(
