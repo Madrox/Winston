086e8bdd4d0300e62303a028ada22ec68aff643b
David Horn <david.horn@disney.com>
2017-02-13 23:11:04 -0800

    model reg

--- winston_jobs/admin.py
+++ winston_jobs/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from django.apps import apps
+
+from .models import *
 
 # Register your models here.
+app = apps.get_app_config('winston_jobs')
+for model in app.models.values():
+    name = model.__name__
+    if name in ('Tag', 'Ticket'):
+        continue
+    admin.site.register(model)
