ae653d141293b0f6e8be43d2229f27c2d6e939f4
David Horn <david.horn@disney.com>
2017-02-20 14:34:18 -0800

    add staticfiles_urlpatterns

--- Winston/urls.py
+++ Winston/urls.py
@@ -15,9 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 
 urlpatterns = [
     url(r'^api/', include('winston_api.urls')),
     url(r'^admin/', admin.site.urls),
-]
+] + staticfiles_urlpatterns()
